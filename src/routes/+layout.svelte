f47f459c5b3360202e9e03666ebfdd494cfe60d8
Blaine Landowski <blalan05@gmail.com>
2023-03-04 10:43:44 -0600

    Mobile Nav and CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,33 @@
-<nav class="desktop flex-container">
-  <div class="home"><a class="test" href="/"><img class="logo" src={logo}/></a></div>
+<nav class="desktop-nav flex-container">
+  <div class="home"><a class="test" href="/"><img class="logo" src={logo} alt="FoundTech Logo"/></a></div>
   <div class="menu-button"><a href="/services">Services</a></div>
   <div class="menu-button"><a href="/fullvue">FullVue</a></div>
   <div class="menu-button"><a href="/about">About</a></div>
   <div class="menu-button"><a href="/contacts">Contacts</a></div>
 </nav>
-<!--
-<nav class="mobile flex-container hidden">
-  <div><img class="logo" src={logo}/></div>
+
+<nav class="mobile-nav flex-container">
+  <input type="checkbox" class="nav-toggle" />
+  <div class="static-logo">
+    <img class="logo" src={logo} alt="FoundTech Logo" />
+  </div>
+  <div class=oc-nav-container>
+    <ul>
+      <li><a href="/services">Services</a></li>
+      <li><a href="/fullvue">FullVue</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contacts">Contacts</a></li>
+    </ul>
+  </div>
 </nav>
--->
+
 <div>
   <main>
     <slot></slot>
   </main>
 </div>
 <footer>
-  <div class="logo-no-link"><img class="logo" src={logo}/></div>
+  <div class="logo-no-link"><img class="logo" src={logo} alt="FoundTech Logo"/></div>
 </footer>
 
 <script>
@@ -38,6 +49,9 @@
     display: flex;
   }
 
+  .static-logo {
+    max-width: 250px;
+  }
   .logo {
     width: 100%;
   }
@@ -56,13 +70,42 @@
     text-align: center;
     align-self: center;
   }
-
-  .hidden {
+  
+  .desktop-nav {
     display: none;
+  }
+
+  .mobile-nav {
+    position: relative;
+  }
+  
+  .oc-nav-container {
+    position: absolute;
+    left: 420px;
+    width: 25%;
+    max-width: 420px;
+    background: black;
+  }
+
+  .oc-nav-container li a {
+    color: white;
+  }
+
+  .nav-toggle:checked {
+    left: 0;
   }
 
   .test {
     align-self: center;
     text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    .desktop-nav {
+      display: flex;
+    }
+    .mobile-nav {
+      display: none;
+    }
   }
 </style>
