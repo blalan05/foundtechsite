3556aea11f7fb95ede7aeeb5412698e875e84baf
davidbluhub <daveb86@gmail.com>
2023-03-06 11:38:11 -0500

    work on home page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,48 @@
   <div class="hero">
     <h1>Affordable and Scalable Software Solutions for Small Businesses.</h1>
     <p>
-      We understand that running a small business can be challenging, especially when it comes to managing the day-to-day operations.
-      Thats why we offer a range of user-friendly software tools to help streamline your business processes so you can build on a solid fountation.
+      We understand that running a small business can be challenging, especially when 
+      it comes to managing the day-to-day operations. Thats why we offer a range of 
+      user-friendly software tools to help streamline your business processes so you 
+      can build on a solid fountation.
     </p>
     <button>Our Services</button>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
   </div>
+  <div class="preview-cards">
+    <div><p>Maximize your efficency and profitability</p></div>
+    <div class="card"><h1>1</h1></div>
+    <div class="card"><h1>2</h1></div>
+    <div class="card"><h1>3</h1></div>
+  </div>
+  <div class="FullVue-Ad">
+    <div>
+      <h2>Our Approach</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos soluta nesciunt, 
+        minus vitae blanditiis provident corrupti, suscipit nihil sapiente amet 
+        assumenda fugit unde rem non libero ut. Inventore deleniti at aliquam nesciunt 
+        maiores facere voluptas odio laboriosam eligendi. Tempore doloremque vitae rem 
+        iure corrupti aliquam sed. Eum distinctio, quos ea repellat repudiandae 
+        necessitatibus dignissimos unde quae quam deleniti? Rem, repellat.
+      </p>
+      <button>About Us</button>
+    </div>
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
   <div>
-
+    <h1>Software that is built to last.</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur eos 
+      cupiditate natus. Ex nostrum dolorem vero odio, atque, repellat ad, nam qui 
+      nobis inventore temporibus repellendus consequatur officiis beatae iste.
+    </p>
+    <button>Reach Out Today</button>
   </div>
 </div>
 
@@ -25,5 +59,26 @@
     font-weight: bold;
     letter-spacing: 2%;
   }
+
+  .card {
+    max-width: 768px;
+    height: 1000px;
+    text-align: center;
+    align-self: center;
+    background-color: red;
+  }
+
+  .card:nth-child(3) {
+    background-color: green;
+  }
+
+  .card:nth-child(4) {
+    background-color: blue;
+  }
+/*
+  .card:nth-child(4) {
+    background-color: red;
+  }
+*/
 
 </style>
