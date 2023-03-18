279813f7e6f67f6b5193b272c7db09b0fc471c87
davidbluhub <daveb86@gmail.com>
2023-03-17 21:11:04 -0400

    layout progress on FullVue page

--- src/routes/fullvue/+page.svelte
+++ src/routes/fullvue/+page.svelte
@@ -12,11 +12,11 @@
   </section>
 
   <section class="features-section">
-    <div class="feature-right">
+    <div class="feature">
       <div class="feature-image"></div>
       <div class="feature-text">
         <div class="circle"></div>
-        <div>
+        <div class="text">
           <h2>Feature of the Software</h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio magnam 
@@ -25,15 +25,15 @@
             at autem ipsam fugit nemo? Eligendi nemo impedit qui excepturi, maxime 
             ipsum rerum aspernatur quis voluptatibus! Velit optio veniam laborum. 
             Repudiandae.
-          </p>
+          </p>  
           <button>Get A Quote</button>
         </div>
       </div>
     </div>
-    <div class="feature-left">
+    <div class="feature">
       <div class="feature-text">
         <div class="circle"></div>
-        <div>
+        <div class="text">
           <h2>Feature of the Software</h2>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio magnam 
@@ -70,7 +70,7 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 350vh;
+    height: 360vh;
     width: 100%;
   }
 
@@ -80,12 +80,12 @@
     justify-content: center;
     align-items: center;
     text-align: center;
-    height: 30%;
+    height: 28%;
     width: 60%;
   }
 
   .screenshot {
-    height: 30%;
+    height: 28%;
     width: 70%;
     border-radius: 30px;
     box-shadow: 15px 20px 60px 20px #777777;
@@ -95,7 +95,73 @@
 
   .endorsements {
     display: flex;
-    height: 40%;
+    height: 44%;
+    width: 92%;
+    gap: 4%;
+    justify-content: center;
+    align-items: center;
   }
+
+  .endorsement {
+    height: 65%;
+    width: 50%;
+    background-color: #c0c0c0;
+    border-radius: 30px;
+  }
+
+  .features-section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-style: solid;
+  }
+
+  .feature {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10%;
+    border-style: solid;
+    margin-bottom: 10%;
+  }
+
+  .feature-image {
+    width: 45%;
+    height: 70vh;
+    background-color: #c0c0c0;
+    border-radius: 30px;
+    box-shadow: 10px 15px 45px 10px #777777;
+  }
+
+  .feature-text {
+    width: 55%;
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+    row-gap: 10%;
+    border-style: solid;
+  }
+
+  .circle {
+    height: 115px;
+    width: 115px;
+    background-color: aqua;
+    border-radius: 50%;
+    border-style: solid;
+    border-width: 15px;
+    border-color: lightblue;
+  }
+
+  .text {
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    gap: 10%;
+    border-style: solid;
+  }
+
+
 
 </style>
