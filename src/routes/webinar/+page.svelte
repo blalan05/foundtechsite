9dda95aed5c1e80d7d3f24ecf2d07add59c1eb4f
davidbluhub <daveb86@gmail.com>
2024-01-09 13:08:10 -0500

    added webinar page

---
+++ src/routes/webinar/+page.svelte
@@ -0,0 +1,73 @@
+<div>
+  <section class="section-one">
+    <h2>Unlock Insights, Drive Growth</h2>
+    <h3>Register for the FullVue Webinar</h3>
+    <p>Thursday 25th January, 2024</p>
+    <p>11:00 AM (CST)</p>
+  </section>
+  <section class="section-two">
+    <div>
+      <h3>Welcome to Foundational Technologies FullVue Webinar!</h3>
+      <p>Are you ready to revolutionize the way you harness data for business success? Join us for our exclusive webinar designed to empower businesses like yours with actionable insights and strategies.</p>
+      <div class="why-attend">
+        <p>Why Attend Our Webinar?</p>
+        <ul class="benefits-list">
+          <li><b>Gain Actionable Insights:</b> Learn how <b>FullVue's</b> comprehensive tools can transform data into actionable insights that drive informed decision-making for your business.</li>
+          <li><b>Enhance Operational Efficiency:</b> Discover how our platform streamlines operations, saving you time and resources while maximizing productivity.</li>
+          <li><b>Harness Real-Time Data Visualization:</b> Explore the power of real-time data visualization and its impact on keeping your business agile in today's fast-paced market landscape.</li>
+        </ul>
+      </div>
+      <p>Don't miss this opportunity to propel your business forward. Register now to secure your spot for this insightful webinar!</p>
+      <a href="/contact">Register Now!</a>
+    </div>
+  </section>
+  <section class="section-three">
+    <p>Webinar Details</p>
+    <ul>
+      <li>Date: January 25, 2024</li>
+      <li>Time: 11:00 AM CST</li>
+      <li>Duration: 30 Minutes</li>
+    </ul>
+    <p>Can't attend live? No worries! Registering guarantees you'll receive the webinar recordings afterward, ensuring you don't miss out on valuable insights.</p>
+    <p>We're excited to have you join us on this journey toward unlocking powerful business insights with FullVue!</p>
+  </section>
+</div>
+
+<style>
+h1 {
+  font-size: 64px;
+  font-weight: bold;
+  letter-spacing: 2%;
+}
+
+h2 {
+  font-size: 32px;
+  font-weight: bold;
+  letter-spacing: 2%;
+}
+
+h3 {
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: 2%;
+}
+
+.section-one {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+
+.section-two {
+  width: 60%;
+}
+
+ul {
+  list-style: disc;
+}
+
+.why-attend {
+  padding-left: 30px;
+}
+
+</style>
