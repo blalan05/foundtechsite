309ded94d72a92ee2030c65306a6529b7e4f0b3a
davidbluhub <daveb86@gmail.com>
2024-01-15 18:39:06 -0500

    webinar page and nav finished

--- src/routes/webinar/+page.svelte
+++ src/routes/webinar/+page.svelte
@@ -8,21 +8,32 @@
     </div>
   </section>
   <section class="section-two">
-    <div class="section-two-content">
-      <div class="why-attend">
-        <p>Welcome to Foundational Technologies FullVue Webinar!</p>
-        <p>Are you ready to revolutionize the way you harness data for business success? Join us for our exclusive webinar designed to empower businesses like yours with actionable insights and strategies.</p>
-        <p>Why Attend Our Webinar?</p>
-        <ul class="benefits-list">
-          <li><b>Gain Actionable Insights:</b> Learn how <b>FullVue's</b> comprehensive tools can transform data into actionable insights that drive informed decision-making for your business.</li>
-          <li><b>Enhance Operational Efficiency:</b> Discover how our platform streamlines operations, saving you time and resources while maximizing productivity.</li>
-          <li><b>Harness Real-Time Data Visualization:</b> Explore the power of real-time data visualization and its impact on keeping your business agile in today's fast-paced market landscape.</li>
-        </ul>
-        <p>Don't miss this opportunity to propel your business forward. Register now to secure your spot for this insightful webinar!</p>
+    <div class="section-two-content-bg">
+      <div class="section-two-content">
+        <div class="why-attend">
+          <p>Welcome to Foundational Technologies FullVue Webinar!</p>
+          <p>Are you ready to revolutionize the way you harness data for business success? Join us for our exclusive webinar designed to empower businesses like yours with actionable insights and strategies.</p>
+          <p>Why Attend Our Webinar?</p>
+          <ul class="benefits-list">
+            <li><b>Gain Actionable Insights:</b> Learn how <b>FullVue's</b> comprehensive tools can transform data into actionable insights that drive informed decision-making for your business.</li>
+            <li><b>Enhance Operational Efficiency:</b> Discover how our platform streamlines operations, saving you time and resources while maximizing productivity.</li>
+            <li><b>Harness Real-Time Data Visualization:</b> Explore the power of real-time data visualization and its impact on keeping your business agile in today's fast-paced market landscape.</li>
+          </ul>
+        </div>
       </div>
-      <div class="sign-up">
-        <a class="register-button" href="/contact">Register Now!</a>
-      </div>
+    </div>
+    <div class="sign-up-container">
+      <p>Don't miss this opportunity to propel your business forward. Register now to secure your spot for this insightful webinar!</p>
+      <form name="sign-up" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact">
+        <input type="text" class="first-name" name="firstName" placeholder="First Name*">
+        <input type="text" class="last-name" name="lastName" placeholder="Last Name*">
+        <input type="text" class="company" name="company" placeholder="Company*">
+        <input type="email" class="email" name="businessEmail" placeholder="Email*">
+        <div class="register-btn-container">
+          <input class="register-btn" type="submit" value="Register">
+        </div>
+      </form>
     </div>
   </section>
   <section class="section-three">
@@ -60,7 +71,7 @@ h3 {
 
 ul {
   list-style: disc;
-  padding-left: 1rem;
+  padding-left: 2rem;
 }
 
 li {
@@ -73,7 +84,6 @@ li {
   justify-content: center;
   background-image: linear-gradient(
     175deg, 
-    white, 
     white 80%, 
     var(--blue), 
     var(--blue) 20%
@@ -91,11 +101,22 @@ li {
 }
 
 .section-two {
+  color: white;
+  background-image: url('$lib/assets/Homepage-4.webp'), linear-gradient(
+    175deg, 
+    var(--blue) 80%,
+    white 5% 
+  );
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: bottom;
+}
+
+.section-two-content-bg {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: var(--blue);
-  color: white;
 }
 
 .section-two-content {
@@ -106,6 +127,63 @@ li {
 .section-two-content p {
   padding: 1rem 0 1rem 0;
   font-weight: bold;
+}
+
+.sign-up-container {
+  text-align: center;
+  width: 100%;
+  padding: 3rem 0 2em 0;
+}
+
+.sign-up-container p {
+  padding: 1rem 0 1rem 0;
+  font-weight: bold;
+  border-top: solid;
+  margin: 0 auto;
+  width: 90%
+}
+
+form {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    "first-name"
+    "last-name"
+    "company"
+    "email"
+    "btn-container";
+  text-align: center;
+  gap: 1em;
+  margin: 0 auto;
+}
+
+form input {
+  padding: 1em;
+  width: 80%;
+  margin: auto;
+  font-size: 1em;
+  border-radius: 4px;
+  border-style: solid;
+  border-width: 1px;
+}
+
+.register-btn-container {
+  grid-area: btn-container;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.register-btn {
+  width: 35%;
+  margin: auto;
+  box-shadow: none;
+  background-color: var(--green);
+  color: white;
+  padding: .5em 0;
+  font-weight: bold;
+  font-size: 1em;
 }
 
 .section-three {
@@ -125,6 +203,54 @@ li {
   font-weight: bold;
 }
 
+@media (min-width: 768px) {
+  .section-one {
+    background-image: linear-gradient(
+    175deg, 
+    white 75%, 
+    var(--blue), 
+    var(--blue) 20%
+    );
+  }
 
+  form {
+    max-width: 1000px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: auto;
+    grid-template-areas:
+      "email phone-number"
+      "first-name last-name"
+      "textarea textarea"
+      "btn-container btn-container"
+  }
+
+  .register-btn {
+    width: 25%;
+  }
+}
+
+@media (min-width: 1201px) {
+  h3 {
+    font-size: 48px;
+  }
+
+  p {
+    font-size: 24px;
+  }
+
+  ul {
+    font-size: 24px;
+  }
+
+  .section-one {
+    height: 50vh;
+    background-image: linear-gradient(
+    175deg, 
+    white 70%, 
+    var(--blue), 
+    var(--blue) 20%
+    );
+  }
+}
 
 </style>
