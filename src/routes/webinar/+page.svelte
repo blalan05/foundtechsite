5f3b11be6a9a26eca07caa67c2ff1dcd776035a4
davidbluhub <daveb86@gmail.com>
2024-01-15 09:45:36 -0500

    preliminary mobile layout complete

--- src/routes/webinar/+page.svelte
+++ src/routes/webinar/+page.svelte
@@ -1,7 +1,7 @@
 <div>
   <section class="section-one">
     <div class="section-one-content">
-      <h2>Unlock Insights,<br/> Drive Growth</h2>
+      <h3>Unlock Insights, Drive Growth!</h3>
       <h3>Register for the FullVue Webinar</h3>
       <p>Thursday 25th January, 2024</p>
       <p>11:00 AM (CST)</p>
@@ -9,27 +9,29 @@
   </section>
   <section class="section-two">
     <div class="section-two-content">
-      <p>Welcome to Foundational Technologies FullVue Webinar!</p>
-      <p>Are you ready to revolutionize the way you harness data for business success? Join us for our exclusive webinar designed to empower businesses like yours with actionable insights and strategies.</p>
       <div class="why-attend">
+        <p>Welcome to Foundational Technologies FullVue Webinar!</p>
+        <p>Are you ready to revolutionize the way you harness data for business success? Join us for our exclusive webinar designed to empower businesses like yours with actionable insights and strategies.</p>
         <p>Why Attend Our Webinar?</p>
         <ul class="benefits-list">
           <li><b>Gain Actionable Insights:</b> Learn how <b>FullVue's</b> comprehensive tools can transform data into actionable insights that drive informed decision-making for your business.</li>
           <li><b>Enhance Operational Efficiency:</b> Discover how our platform streamlines operations, saving you time and resources while maximizing productivity.</li>
           <li><b>Harness Real-Time Data Visualization:</b> Explore the power of real-time data visualization and its impact on keeping your business agile in today's fast-paced market landscape.</li>
         </ul>
+        <p>Don't miss this opportunity to propel your business forward. Register now to secure your spot for this insightful webinar!</p>
       </div>
-      <p>Don't miss this opportunity to propel your business forward. Register now to secure your spot for this insightful webinar!</p>
-      <a href="/contact">Register Now!</a>
+      <div class="sign-up">
+        <a class="register-button" href="/contact">Register Now!</a>
+      </div>
     </div>
   </section>
   <section class="section-three">
     <div class="section-three-content">
       <p>Webinar Details</p>
       <ul>
-        <li>Date: January 25, 2024</li>
-        <li>Time: 11:00 AM CST</li>
-        <li>Duration: 30 Minutes</li>
+        <li><b>Date:</b> January 25, 2024</li>
+        <li><b>Time:</b> 11:00 AM CST</li>
+        <li><b>Duration:</b> 30 Minutes</li>
       </ul>
       <p>Can't attend live? No worries! Registering guarantees you'll receive the webinar recordings afterward, ensuring you don't miss out on valuable insights.</p>
       <p>We're excited to have you join us on this journey toward unlocking powerful business insights with FullVue!</p>
@@ -58,13 +60,24 @@ h3 {
 
 ul {
   list-style: disc;
-  padding-left: 20px;
+  padding-left: 1rem;
+}
+
+li {
+  padding: .25rem 0 .25rem .5rem;
 }
 
 .section-one {
   height: 40vh;
   display: flex;
   justify-content: center;
+  background-image: linear-gradient(
+    175deg, 
+    white, 
+    white 80%, 
+    var(--blue), 
+    var(--blue) 20%
+  );
 }
 
 .section-one-content {
@@ -81,11 +94,18 @@ ul {
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: var(--blue);
+  color: white;
 }
 
 .section-two-content {
   flex-direction: column;
-  width: 50%;
+  width: 85%;
+}
+
+.section-two-content p {
+  padding: 1rem 0 1rem 0;
+  font-weight: bold;
 }
 
 .section-three {
@@ -95,8 +115,14 @@ ul {
 }
 
 .section-three-content {
+  padding: 1rem 0 1rem 0;
   flex-direction: column;
-  width: 50%;
+  width: 85%;
+}
+
+.section-three-content p {
+  padding: 1rem 0 1rem 0;
+  font-weight: bold;
 }
 
 
