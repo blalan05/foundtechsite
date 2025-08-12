4b0267b19caa8a9235528df51f0d5fe909b89f26
Blaine Landowski <blalan05@gmail.com>
2025-08-12 17:25:56 -0500

    fix build?

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		}),
 		prerender: {
 			crawl: false,
-			entries: ['/', '/about', '/construction', '/services', '/contact', '/webinar', '/privacy-policy', '/products', '/products/inventory-importer']
+			entries: ['/', '/about', '/construction', '/services', '/contact', '/webinar', '/privacy-policy', '/products', '/products/inventory-importer', '/fullvue']
 		}
 	}
 };
